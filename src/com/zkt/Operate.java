06e59ff4f39c4db7c503e557aeebc08b95ad4db3
Zkt3 <2313726595@qq.com>
2025-12-27 12:23:28 +0800

    faet: 新增核心业务类

---
+++ src/com/zkt/Operate.java
@@ -0,0 +1,105 @@
+package com.zkt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 核心业务类
+ */
+public class Operate {
+    private List<Person> list;
+    public Operate() {
+        this.list = new ArrayList<>();
+    }
+
+    //添加业务逻辑
+    public void addLogic(){
+
+    }
+
+    //查询业务逻辑
+    public void searchLogic(){
+
+    }
+
+    //修改业务逻辑
+    public void modifyLogic(){
+
+    }
+
+    //删除业务逻辑
+    public void deleteLogic(){
+
+    }
+
+    //排序业务逻辑
+    public void orderLogic(){
+
+    }
+
+    //添加新纪录信息
+    public void addOperation(){
+
+    }
+
+    //查询全部记录
+    public void showAll(){
+
+    }
+
+    //按姓名查找
+    public void searchByName(){
+
+    }
+
+    //按年龄查找
+    public void searchByAge(){
+
+    }
+
+    //按性别查找
+    public void searchBySex(){
+
+    }
+
+    //按电话号码查找
+    public void searchByTelNum(){
+
+    }
+
+    //按地址查找
+    public void searchByAddress(){
+
+    }
+
+    //修改指定记录
+    public void modifyOperation(){
+
+    }
+
+    //删除指定记录
+    public void deleteOperation(){
+
+    }
+
+    //删除全部记录
+    public void deleteAllOperation(){
+
+    }
+
+    //按姓名排序
+    public void orderByName(){
+
+    }
+
+    //按年龄排序
+    public void orderByAge(){
+
+    }
+
+    //按性别排序
+    public void orderBySex(){
+
+    }
+
+}
