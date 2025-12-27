5a91faffa936c4679507bf826d0fd4208ea6f48b
Zkt3 <2313726595@qq.com>
2025-12-27 12:29:38 +0800

    feat: 新增校验器类

---
+++ src/com/zkt/TelNoteRegex.java
@@ -0,0 +1,33 @@
+package com.zkt;
+
+public class TelNoteRegex {
+    //对菜单项校验
+    public int menuItemValidate(int min,int max){
+        return 0;
+    }
+
+    //对姓名校验
+    public String nameValidate(){
+        return null;
+    }
+
+    //对年龄校验
+    public String ageValidate(){
+        return null;
+    }
+
+    //对性别校验
+    public String sexValidate(){
+        return null;
+    }
+
+    //对电话号码校验
+    public String telNumValidate() {
+        return null;
+    }
+
+    //对地址校验
+    public String addressValidate(){
+        return null;
+    }
+}
