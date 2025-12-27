d87d8a55667b98c002176277581356f512abeed3
Zkt3 <2313726595@qq.com>
2025-12-27 12:03:26 +0800

    feat:新增菜单类

---
+++ src/com/zkt/Menu.java
@@ -0,0 +1,82 @@
+package com.zkt;
+
+/**
+ * 生成项目中所有的菜单
+ */
+public class Menu {
+    //主页面
+    public void mainMenu(){
+        System.out.println("------------");
+        System.out.println("**  1 添加记录  **");
+        System.out.println("**  2 查找记录  **");
+        System.out.println("**  3 修改记录  **");
+        System.out.println("**  4 删除记录  **");
+        System.out.println("**  5 排序记录  **");
+        System.out.println("**  6 退出记录  **");
+        System.out.println("------------");
+    }
+
+    //添加页面
+    public void addMenu(){
+        System.out.println("------------");
+        System.out.println("**  1 添加新纪录  **");
+        System.out.println("**  2 查找全记录  **");
+        System.out.println("**  3 返回上一级  **");
+        System.out.println("------------");
+    }
+
+    //查找页面
+    public void searchMenu(){
+        System.out.println("------------");
+        System.out.println("**  1 按姓名查找  **");
+        System.out.println("**  2 按年龄查找  **");
+        System.out.println("**  3 按性别查找  **");
+        System.out.println("**  4 按电话号码查找  **");
+        System.out.println("**  5 按地区查找  **");
+        System.out.println("**  6 查找全纪录  **");
+        System.out.println("**  7 返回上一级  **");
+        System.out.println("------------");
+    }
+
+    //修改页面
+    public void modifyMenu(){
+        System.out.println("------------");
+        System.out.println("**  1 查看全纪录  **");
+        System.out.println("**  2 修改指定记录  **");
+        System.out.println("**  3 返回上一级  **");
+        System.out.println("------------");
+    }
+
+    //修改子页面
+    public void subModifyMenu(){
+        System.out.println("------------");
+        System.out.println("**  1 修改姓名  **");
+        System.out.println("**  2 修改年龄  **");
+        System.out.println("**  3 修改性别  **");
+        System.out.println("**  4 修改电话号码  **");
+        System.out.println("**  5 修改地区  **");
+        System.out.println("**  6 返回上一级  **");
+        System.out.println("------------");
+    }
+
+    //删除页面
+    public void deleteMenu(){
+        System.out.println("------------");
+        System.out.println("**  1 查看全纪录  **");
+        System.out.println("**  2 删除指定记录  **");
+        System.out.println("**  2 删除全部记录  **");
+        System.out.println("**  4 返回上一级  **");
+        System.out.println("------------");
+    }
+
+    //排序页面
+    public void orderMenu(){
+        System.out.println("------------");
+        System.out.println("**  1 按姓名排序  **");
+        System.out.println("**  2 按年龄排序  **");
+        System.out.println("**  3 按性别排序  **");
+        System.out.println("**  4 查看全纪录  **");
+        System.out.println("**  5 返回上一级  **");
+        System.out.println("------------");
+    }
+}
