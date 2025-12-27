9936f50f8b1dafab7ddbddcd1865227eecd7b898
Zkt3 <2313726595@qq.com>
2025-12-27 11:48:02 +0800

    feat: 新增电话本项目入口类

---
+++ src/com/zkt/App.java
@@ -0,0 +1,21 @@
+package com.zkt;
+
+/**
+ * 电话本项目入口类
+ */
+public class App {
+    /**
+     * 启动电话本项目
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 控制主菜单
+     */
+    public void start(){
+
+    }
+}
